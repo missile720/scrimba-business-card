190242d82b8b2c698f3de1f23fa94d699dba2304
Misael <misaelbolainez@gmail.com>
2023-04-29 13:51:22 -0700

    fix(links)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import "./Footer.css"
 function Footer() {
   return (
     <div className="footer">
-      <img src="../../public/images/twitter.png" alt="" />
-      <img src="../../public/images/facebook.png" alt="" />
-      <img src="../../public/images/instagram.png" alt="" />
-      <img src="../../public/images/github.png" alt="" />
+      <img src="/images/twitter.png" alt="" />
+      <img src="/images/facebook.png" alt="" />
+      <img src="/images/instagram.png" alt="" />
+      <img src="/images/github.png" alt="" />
     </div>
   )
 }
