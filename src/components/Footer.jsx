dd391e868c7dab2958e254def9e1163dc33163f8
Misael <misaelbolainez@gmail.com>
2023-04-29 13:45:32 -0700

    feat(navbar): finished adding nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import "./Footer.css"
 function Footer() {
   return (
     <div className="footer">
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
+      <img src="../../public/images/twitter.png" alt="" />
+      <img src="../../public/images/facebook.png" alt="" />
+      <img src="../../public/images/instagram.png" alt="" />
+      <img src="../../public/images/github.png" alt="" />
     </div>
   )
 }
